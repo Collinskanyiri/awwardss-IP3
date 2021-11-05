d228257cbab4e395319727673b6ec980007e415f
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-05 12:21:23 +0300

    add routes

--- aawardss/urls.py
+++ aawardss/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
+router.register('profile', views.ProfileViewSet)
 
 ]
