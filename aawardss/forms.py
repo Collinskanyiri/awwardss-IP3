4d328dd8ec83381daee4103d11b7893b0c6162ab
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-05 12:14:01 +0300

    add signup and post form

--- aawardss/forms.py
+++ aawardss/forms.py
@@ -4,6 +4,20 @@ from django.contrib.auth.models import User
 from .models import Post, Profile, Rating
 from cloudinary.models import CloudinaryField
 
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class PostForm(forms.ModelForm):
+    photo = CloudinaryField('image')
+
+    class Meta:
+        model = Post
+        fields = ('photo', 'title', 'url', 'description', 'technologies',)
 
 
 
